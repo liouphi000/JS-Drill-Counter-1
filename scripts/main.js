c9fad7a6349bd0e8572c69c77dcea1badf60102e
Phillip Liou <Phillip Liou>
2017-02-25 21:01:18 -0600

    Hopefully did it right

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,28 @@
 //Create a variable called "count"
-
-function increaseCount () {
+var count = 0
+function increaseCount (num) {
+	return count++
   //Define a function that increases the count variable
-
 }
 
-function decreaseCount() {
+function decreaseCount(num) {
+	return count--
+} 
+if (count>= 0) {
+	alert("Please Don't Do That")
+}
+
   /* Define a function that decreases the count.
   DO NOT allow the count to go below 0. If the user tries to decrease the count at 0,
   create a popup that warns them not to do that. */
 
-}
 
-function logCountInConsole() {
+var max = 100
+function logCountInConsole(number) {
+	for (var i === count; i<max; i) {
+		console.log(i)
+	}
+
   //Define a function that console.logs a message as many times as the count says.
   //For example, if the count is 6, it should log 6 times.
 
